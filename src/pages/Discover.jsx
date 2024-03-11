8c046aa24e65771f7d19c9f40ca0e34444b1912c
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-11 11:51:42 +0530

    used shazam api from rapid api and it fetches songs from api

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,15 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
 const Discover = () => {
-  //   console.log(genres);
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genresTitle = "Pop";
+  console.log(data);
+
+  if (isFetching) return <Loader title="loading songs..." />;
+  if (error) return <Error />;
+
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -22,7 +29,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7].map((song, i) => (
+        {data?.tracks.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,8 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vkjsnK4bKzuUzVLza",
-    "X-RapidAPI-Host": "",
+    "X-RapidAPI-Key": "3884d3808fmsheb555fa9cda0c6fp1805aajsna701b39d8757",
+    // "X-RapidAPI-Host": "",
+    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
   },
 };
 // fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
@@ -15,15 +16,27 @@ const options = {
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com/",
+    // baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    baseUrl: "https://shazam.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLza"
+        "3884d3808fmsheb555fa9cda0c6fp1805aajsna701b39d8757"
         // import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
       );
 
       return headers;
     },
   }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/track" }),
+    // getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    // getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    // getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    // getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    // getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    // getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+  }),
 });
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
