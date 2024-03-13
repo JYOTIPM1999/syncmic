f4944bfe29023cafa7b22212b97dfbf5e0c95b18
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-13 10:44:04 +0530

    Try to fetch lyrics from API

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,21 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useDispatch, useSelector } from "react-redux";
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { songid } = useParams();
+
+  return (
+    <div className="flex flex-col">
+      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+        <div className="mt-5"></div>
+      </div>
+    </div>
+  );
+};
 
 export default SongDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,22 +1,44 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+// const options = {
+//   method: "GET",
+//   params: {
+//     id: "296831279",
+//     l: "en-US",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "3884d3808fmsheb555fa9cda0c6fp1805aajsna701b39d8757",
+//     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+//   },
+// };
+// fetch("https://shazam.p.rapidapi.com/songs/v2/get-details", options)
+//   // .then((res) => res.json())
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
 
 const options = {
   method: "GET",
+  url: "https://shazam.p.rapidapi.com/songs/v2/get-details",
+  params: {
+    id: "1217912247",
+    l: "en-US",
+  },
   headers: {
     "X-RapidAPI-Key": "3884d3808fmsheb555fa9cda0c6fp1805aajsna701b39d8757",
-    // "X-RapidAPI-Host": "",
     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
   },
 };
-// fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-//   .then((res) => res.json())
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err));
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data.data[0]);
+} catch (error) {
+  console.error(error);
+}
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     baseUrl: "https://shazam.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set(
@@ -30,11 +52,13 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/track" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/track/info?track_id=${songid}`,
+    }),
     // getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
     // getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
     // getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     // getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
-    // getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
     // getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
   }),
 });
