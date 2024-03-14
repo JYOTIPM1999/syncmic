e72d3f4ee06b1ad48538b9d359884ca267cd5f55
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-14 19:08:52 +0530

    Top artists completed

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -17,7 +17,7 @@ const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
   // console.log(song);
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slidup  rounded-lg cursor-pointer ">
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup  rounded-lg cursor-pointer ">
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
